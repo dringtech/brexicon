8781fb51a2b923964b06202edbd3de02939de34a
Giles Dring <giles@dringtech.com>
2017-11-30 15:52:20 +0000

    Providing random brexample on the homepage

---
+++ _includes/random_word.js
@@ -0,0 +1,13 @@
+{% assign brexit_words = site.brexicon | map: "slug" %}
+var brexitWords = [
+  {% for word in site.brexicon %}
+  {
+    word: "{{ word.title }}", pos: "{{ word.type | first }}"
+  }{% if forloop.last != true %},{% endif %}
+  {% endfor %}
+];
+var size = brexitWords.length;
+var i = Math.floor(Math.random()*size);
+document.write('<div class="brentry"><div class="term"><div class="word">' +
+  brexitWords[i].word + '</div> ('+ brexitWords[i].pos +
+  ')</div><div class="definition">'+brexitWords[i].word +'</div></div>');
