740bd12b33de235fcf9ce12ed73a0ba265b5497b
Giles Dring <giles@dringtech.com>
2017-11-30 16:13:44 +0000

    Link to random word

--- _includes/random_word.js
+++ _includes/random_word.js
@@ -1,13 +1,9 @@
 {% assign brexit_words = site.brexicon | map: "slug" %}
 var brexitWords = [
-  {% for word in site.brexicon %}
-  {
-    word: "{{ word.title }}", pos: "{{ word.type | first }}"
-  }{% if forloop.last != true %},{% endif %}
-  {% endfor %}
+{% for word in site.brexicon %}{word: "{{ word.title }}", url: "{{ word.url }}", pos: "{{ word.type | first }}"}{%- if forloop.last != true %},{% endif %}{% endfor %}
 ];
 var size = brexitWords.length;
 var i = Math.floor(Math.random()*size);
-document.write('<div class="brentry"><div class="term"><div class="word">' +
-  brexitWords[i].word + '</div> ('+ brexitWords[i].pos +
+document.write('<div class="brentry"><div class="term"><div class="word"><a href="' + brexitWords[i].url +'">' +
+  brexitWords[i].word + '</a></div> ('+ brexitWords[i].pos +
   ')</div><div class="definition">'+brexitWords[i].word +'</div></div>');
